76571d7e611c2543c706df4a5f8877da1d1a13c3
Yoon <yoon6763@naver.com>
2023-01-11 18:00:36 +0900

    Django myboard 프로젝트 생성

---
+++ mytodo/todo/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+from todo.models import Todo
+
+
+class TodoSimpleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ('id', 'title', 'complete', 'important')
+
+
+class TodoDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ('id', 'title', 'description', 'created', 'complete', 'important')
+
+
+class TodoCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ('title', 'description', 'important')
