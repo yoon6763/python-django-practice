8a90b4c450d70c800c92846da34ce773d60bb2ff
uknow-laptop <yoon6763@naver.com>
2023-01-17 18:03:03 +0900

    realMyBoardProject Create

---
+++ realmyboard/users/models.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, password, **extra_fields):
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, username, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(username, email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    username = models.CharField(max_length=20, unique=True)
+    email = models.EmailField(_('email address'), unique=True)
+    nickname = models.CharField(max_length=20, unique=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', 'nickname']
+
+    def __str__(self):
+        return self.email
