4181e0174690334af17a4affd62d0400c45f0e97
uknow-laptop <yoon6763@naver.com>
2023-02-03 17:39:08 +0900

    not using workspace remove

--- realmyboard/posts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from posts.views import PostView, like_post
-
-urlpatterns = [
-    path('', PostView.as_view()),
-    path('<int:pk>/', PostView.as_view()),
-    path('like/<int:pk>/', like_post, name='like_post'),
-]
--- realmyboard/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from users.views import RegisterView, LoginView, UserView
-
-urlpatterns = [
-    path('register/', RegisterView.as_view()),
-    path('login/', LoginView.as_view()),
-    path('<int:pk>/', UserView.as_view()),
-]
